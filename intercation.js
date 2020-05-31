02dabd10af0c7493a2e301c6125c3f610a1879fa
Chris <44982457+ChRIisS97@users.noreply.github.com>
2020-05-31 22:14:04 +0200

    Update interaction to v 0.5

--- intercation.js
+++ intercation.js
@@ -9,12 +9,14 @@ function switchGraph(id) {
         document.getElementById("control-value").style.display = "none";
         document.getElementById("active-indikator").style.left = "40%";
         document.getElementById("header-indikator").style.left = "0";
+        document.getElementById("timer-sub").style.display = "block";
     } else if (id === "pool") {
         document.getElementById("outdoor-value").style.display = "none";
         document.getElementById("pool-value").style.display = "block";
         document.getElementById("active-indikator").style.left = "5%";
         document.getElementById("header-indikator").style.left = "0";
         document.getElementById("control-value").style.display = "none";
+        document.getElementById("timer-sub").style.display = "block";
     } else if (id === "pool-values") {
         document.getElementById("outdoor-value").style.display = "none";
         document.getElementById("pool-value").style.display = "block";
@@ -23,12 +25,14 @@ function switchGraph(id) {
     } else if (id === "pool-Values") {
         document.getElementById("header-indikator").style.left = "0";
         document.getElementById("active-indikator").style.display = "block";
+        document.getElementById("timer-sub").style.display = "block";
 
         document.getElementById("control-value").style.display = "none";
         document.getElementById("pool-value").style.display = "block";
     } else if (id === "control") {
         document.getElementById("header-indikator").style.left = "34%";
         document.getElementById("active-indikator").style.display = "none";
+        document.getElementById("timer-sub").style.display = "none";
 
         document.getElementById("outdoor-value").style.display = "none";
         document.getElementById("pool-value").style.display = "none";
@@ -50,6 +54,7 @@ function check(id) {
         element2.classList.add("active");
         document.getElementById("active-indikator").style.display = "block";
     }
+
 }
 
 var btnContainer = document.getElementById("selection");
@@ -65,7 +70,7 @@ for (var i = 0; i < btns.length; i++) {
 //Calculate Value to percentage for Slider
 setInterval(function () {
     var obtained = document.getElementById('temperaturec').innerText;
-    var total = 40;
+    var total = 50;
     var percent = obtained * 100 / total;
     //console.log(percent);
     document.getElementById('sub-slider').style.width = "" + percent + "%";
@@ -76,12 +81,17 @@ setInterval(function () {
 setInterval(function () {
     var value = document.getElementById('temperaturec').innerText;
 
-    if (value <= "20") {
-        return document.getElementById('hubu').innerText = "Wassertemperatur Kalt";
-    } else if (value >= "20.5" && value <= "30") {
-        return document.getElementById('hubu').innerText = "Wassertemperatur Normal";
-    } else if (value >= "31") {
-        return document.getElementById('hubu').innerText = "Wassertemperatur Zu Warm";
+    if (value <= "23.5") {
+        document.getElementById('hubu').innerText = "Zum Baden längeren Baden nicht optimal";
+        document.getElementById('hubu').style.color = "rgba(0,119,157,0.4)";
+        return document.getElementById('hubu-text').innerText = "Optimaler Schwimmbereich liegt bei ungefähren 25-27 Grad, da der eigene Körper eine Temperatur von 27 grad besitzt und so auch längere Zyklen im Wasser bleiben kann ohne zu unterkühlen.";
+    } else if (value >= "24" && value <= "29") {
+        document.getElementById('hubu').innerText = "Wassertemperatur optimal zum längeren Baden";
+        return document.getElementById('hubu-text').innerText = "Optimaler Schwimmbereich für längere Schwimmzyklen als auch zur Entspannung der eigenen Muskulatur und deren Regeneration.";
+    } else if (value >= "30") {
+        document.getElementById('hubu').innerText = "Abkühlung nur kurze Zeit";
+        document.getElementById('hubu').style.color = "rgba(85, 6, 2, 0.51)";
+        return document.getElementById('hubu-text').innerText = "Zu warmer Schwimmbereich für eine langfristige Erholung des eigenen Körpers aber für einen längeren Aufenthalt im Becken ohne Probleme möglich";
     } else {
         return document.getElementById('hubu').innerText = "Value not correct";
     }
@@ -108,3 +118,63 @@ setInterval(function () {
     xhttp.open("GET", "/temperatures", true);
     xhttp.send();
 }, 2000);
+
+
+
+//timer for how long your pump is on
+var timerVar;
+var timerVar2;
+function counter() {
+    if (document.getElementById('myonoffswitch').checked) {
+
+        timerVar = setInterval(countTimer, 1000);
+        var totalSeconds = 0;
+
+        function countTimer() {
+            ++totalSeconds;
+            var hour = Math.floor(totalSeconds / 3600);
+            var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+            var seconds = totalSeconds - (hour * 3600 + minute * 60);
+            if (hour < 10)
+                hour = "0" + hour;
+            if (minute < 10)
+                minute = "0" + minute;
+            if (seconds < 10)
+                seconds = "0" + seconds;
+            document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+            document.getElementById("timer-sub").innerHTML = hour + ":" + minute + ":" + seconds;
+            document.getElementById("timer").style.color = "#3498db";
+        }
+    } else {
+        document.getElementById("timer").innerHTML = "00:00:00";
+        document.getElementById("timer-sub").innerHTML = "00:00:00";
+        document.getElementById("timer").style.color = "unset";
+        clearTimeout(timerVar);
+    }
+}
+function counter2() {
+    if (document.getElementById('myonoffswitch2').checked) {
+
+        timerVar2 = setInterval(countTimer, 1000);
+        var totalSeconds = 0;
+
+        function countTimer() {
+            ++totalSeconds;
+            var hour = Math.floor(totalSeconds / 3600);
+            var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+            var seconds = totalSeconds - (hour * 3600 + minute * 60);
+            if (hour < 10)
+                hour = "0" + hour;
+            if (minute < 10)
+                minute = "0" + minute;
+            if (seconds < 10)
+                seconds = "0" + seconds;
+            document.getElementById("timer2").innerHTML = hour + ":" + minute + ":" + seconds;
+            document.getElementById("timer2").style.color = "#3498db";
+        }
+    } else {
+        document.getElementById("timer2").innerHTML = "00:00:00";
+        document.getElementById("timer2").style.color = "unset";
+        clearTimeout(timerVar2);
+    }
+}
