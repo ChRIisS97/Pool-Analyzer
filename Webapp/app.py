311a7548a7165a572dfe86a2f1927a2bafaa725b
Chris <44982457+ChRIisS97@users.noreply.github.com>
2020-08-02 14:24:32 +0200

    Add Main Files

---
+++ Webapp/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+
+from ds18b20 import readTempLines
+from ds18b20 import readTempSensor
+from ds18b20 import sensor
+from ds18b20 import sensor2
+from ds18b20 import sensor3
+
+def read_sensor():
+    temp_one = readTempSensor(sensor)
+    temp_one = str(readTempLines(sensor)[0])
+    return(temp_one)
+
+def read_sensor_two():
+    temp_two = readTempSensor(sensor2)
+    temp_two = str(readTempLines(sensor2)[0])
+    return(temp_two)
+
+def read_sensor_three():
+    temp_three = readTempSensor(sensor3)
+    temp_three = str(readTempLines(sensor3)[0])
+    return(temp_three)
+
+app = Flask(__name__)
+
+@app.route('/getDataFromSensor')
+def read_sensor():
+    temp_one = readTempSensor(sensor)
+    temp_one = str(readTempLines(sensor)[0])
+    return(temp_one)
+
+def read_sensor_two():
+    temp_two = readTempSensor(sensor2)
+    temp_two = str(readTempLines(sensor2)[0])
+    return(temp_two)
+
+def read_sensor_three():
+    temp_three = readTempSensor(sensor3)
+    temp_three = str(readTempLines(sensor3)[0])
+    return(temp_three)
+
+@app.route('/')
+def index():
+    temp_one = read_sensor()
+    temp_two = read_sensor_two()
+    temp_three =read_ sensor_three()
+
+    return renter_template('index.html', temp_one=temp_one, temp_two=temp_two, temp_three=temp_three)
+
+if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0')
---
+++ Webapp/ds18b20.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+import time, sys
+
+# /sys/bus/w1/devices/deviceID/w1_slave
+sensor = 'your path to the first sensor'
+sensor2 = 'your path to the first sensor2'
+sensor2 = 'your path to the first sensor3'
+
+def readTempSensor(sensorName):
+  f = open(sensorName, 'r')
+  lines = f.readlines()
+  f.close
+  return lines
+
+def readTempLines(sensorName)
+  lines = readTempSensor(sensorName)
+
+while lines[0].strip()[-3:] != 'YES':
+  time.sleep(0.2)
+  lines = readTempSensor(sensorName)
+  temperaturStr = lines[1].find('t=')
+
+if temperaturStr != -1:
+  tempData = lines[1][temperaturStr+2:]
+  tempCelsius = float(tempData)/1000.0
+  return [tempCelsius]
