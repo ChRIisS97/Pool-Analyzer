77e1dcaa6f47a60b30cea1763fa2fd8fa2c1d00d
Chris <44982457+ChRIisS97@users.noreply.github.com>
2020-08-21 17:17:03 +0200

    Update app.py

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,53 +1,65 @@
 from flask import Flask, render_template
-
 from ds18b20 import readTempLines
 from ds18b20 import readTempSensor
 from ds18b20 import sensor
 from ds18b20 import sensor2
 from ds18b20 import sensor3
 
+
 def read_sensor():
     temp_one = readTempSensor(sensor)
     temp_one = str(readTempLines(sensor)[0])
-    return(temp_one)
+    return temp_one
+
 
 def read_sensor_two():
     temp_two = readTempSensor(sensor2)
     temp_two = str(readTempLines(sensor2)[0])
-    return(temp_two)
+    return temp_two
+
 
 def read_sensor_three():
     temp_three = readTempSensor(sensor3)
     temp_three = str(readTempLines(sensor3)[0])
-    return(temp_three)
+    return temp_three
+
 
 app = Flask(__name__)
+
 
 @app.route('/getDataFromSensor')
 def read_sensor():
     temp_one = readTempSensor(sensor)
     temp_one = str(readTempLines(sensor)[0])
-    return(temp_one)
+    return temp_one
+
 
 @app.route('/getDataFromSensor2')
 def read_sensor_two():
     temp_two = readTempSensor(sensor2)
     temp_two = str(readTempLines(sensor2)[0])
-    return(temp_two)
+    return temp_two
+
 
 @app.route('/getDataFromSensor3')
 def read_sensor_three():
     temp_three = readTempSensor(sensor3)
     temp_three = str(readTempLines(sensor3)[0])
-    return(temp_three)
+    return temp_three
+
 
 @app.route('/')
 def index():
     temp_one = read_sensor()
     temp_two = read_sensor_two()
-    temp_three =read_ sensor_three()
-
+    temp_three = read_sensor_three()
     return renter_template('index.html', temp_one=temp_one, temp_two=temp_two, temp_three=temp_three)
 
+
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, use_reloader=False, host='0.0.0.0')
+
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='favicon.ico'))
+app.add_url_rule('/apple-touch-icon.png',
+                 redirect_to=url_for('static', filename='apple-touch-icon.png'))
